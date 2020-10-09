352800883c45458e647c217390df0b9260e92df0
romheloise <romheloise@gmail.com>
2020-10-09 13:57:37 +0200

    mouseove working

--- msquare.js
+++ msquare.js
@@ -1,44 +1,47 @@
 class Square {
-    constructor(x, y, color,index) {
+    constructor(x, y,color,num) {
         this.x = x;
         this.y = y;
         this.color = color;
-        this.index = index;
+        this.num = num;
     }
 
     create() {
-        let base = document.querySelector('.cir');
         let square = document.createElement('div');
-        square.classList = `square${this.index}`;
+        square.classList = `square${this.num}`;
         square.style.top = this.y + 'px';
         square.style.left = this.x + 'px';
         square.style.backgroundColor = this.color;
-        base.appendChild(square);
+        document.body.appendChild(square);
+        selector.push(square);
     }
 
     follow(div) {
-        window.addEventListener('mousemove', () => this.stick(div))
+        div.addEventListener('mouseover',(e) => this.stick(e.target))
     }
 
-    stick(div) {
-        console.log(div)
-    }
+    stick(rond) {
+        window.addEventListener('mousemove', (e) => {
+            rond.style.top = e.clientY + 'px';
+            rond.style.left = e.clientX + 'px';
+        })
+    }    
 
 }
 
-
+let selector = [];
 let randomColor = [];
-
 for (let index = 0; index < 10; index++) {
     for (let i = 0; i < 3; i++) {
         randomColor.push((Math.floor(Math.random() * 254) + 1));
     }    
     let randomX = (Math.floor(Math.random() * 800) + 1);
-    let randomY = (Math.floor(Math.random() * 500) + 1);
+    let randomY = (Math.floor(Math.random() * 500) + 1);   
     let circle = new Square(randomX, randomY,`rgb(${randomColor[0]},${randomColor[1]},${randomColor[2]})`,index);
-    circle.create();
-    let selector = document.querySelector(`.square${index}`);
-    console.log(selector) 
+    circle.create(); 
+    circle.follow(selector[index])     
     randomColor = [];
 }
+
+
 
