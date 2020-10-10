4df5ed85b0321c151b43383fab5cb9573cb6f2db
romheloise <romheloise@gmail.com>
2020-10-10 11:46:44 +0200

    WOOOUHHOUOU

--- msquare.js
+++ msquare.js
@@ -27,25 +27,28 @@ class Circle {
         div.addEventListener('mouseover', (e) => {
             this.stick(e.target);
             if (this.currenttouch === 0) {
+                queue.push(this);
                 cumulSize.push(this.divHtml.offsetHeight);
                 this.ballNumber = totalBalls;
                 totalBalls += 1;
             }
             e.target.innerHTML = this.ballNumber;
             this.currenttouch += 1;
-
         })
     }
 
     stick(rond) {
         window.addEventListener('mousemove', (e) => {
             let tempSize = cumulSize.slice(0, this.ballNumber);
-            let fullsize = 0;
+            let fullsize = 0;         
             if (tempSize.length > 0) {
                 fullsize = tempSize.reduce((a, b) => a + b);
-            }            
-            rond.style.top = (e.clientY + fullsize) + 'px';
-            rond.style.left = (e.clientX) + 'px';
+                rond.style.top = window.getComputedStyle(queue[this.ballNumber - 1].divHtml).top;
+                rond.style.left = window.getComputedStyle(queue[this.ballNumber - 1].divHtml).left;
+            } else {
+                rond.style.top = (e.clientY + fullsize) + 'px';
+                rond.style.left = (e.clientX) + 'px';
+            }
         })
     }
 
@@ -63,14 +66,10 @@ class Circle {
 }
 
 
-
+let queue = [];
 let cumulSize = [];
 let totalBalls = 0;
 let randomColor = [];
-
-
-
-
 
 for (let index = 0; index < 15; index++) {
     for (let i = 0; i < 3; i++) {
